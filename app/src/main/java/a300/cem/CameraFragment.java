98f467430bf35aad692d8bcf3e148ad03404affd
Bogdan A. Stanciu <stanciub@uni.coventry.ac.uk>
2018-11-27 15:10:23 +0000

    Logout - button Camera

--- app/src/main/java/a300/cem/CameraFragment.java
+++ app/src/main/java/a300/cem/CameraFragment.java
@@ -1,5 +1,6 @@
 package a300.cem;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -24,9 +25,18 @@ import com.google.firebase.auth.FirebaseAuth;
 import java.io.ByteArrayOutputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Iterator;
 import java.util.List;
 
-public class CameraFragment extends Fragment{
+public class CameraFragment extends Fragment implements SurfaceHolder.Callback {
+
+    Camera camera;
+
+    SurfaceView mSurfaceView;
+    SurfaceHolder mSurfaceHolder;
+
+    //
+    final int CAMERA_REQUEST_CODE = 1;
 
     public static CameraFragment newInstance(){
         CameraFragment fragment = new CameraFragment();
@@ -34,9 +44,89 @@ public class CameraFragment extends Fragment{
     }
 
     @Override
-    public View onCreateView( LayoutInflater inflater,ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.chat_fragment , container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.camera_fragment , container, false);
+
+        mSurfaceView = view.findViewById(R.id.surfaceView);
+        mSurfaceHolder = mSurfaceView.getHolder();
+
+        //Set Permission to access camera
+        if(ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(getActivity(), new String[] {Manifest.permission.CAMERA}, CAMERA_REQUEST_CODE);
+        }else{
+            //Bridge gap between SurfaceHolder and SurfaceView
+            mSurfaceHolder.addCallback(this);
+            mSurfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        }
+
+        Button mLogout = view.findViewById(R.id.logout);
+        mLogout.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                LogOut();
+            }
+        });
+
+
         return view;
 
     }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        camera = Camera.open();
+
+        Camera.Parameters parameters;
+        parameters = camera.getParameters();
+
+        //Set Display Orientation to 90 degrees
+        camera.setDisplayOrientation(90);
+        //Frames per sec - Set to 30
+        parameters.setPreviewFrameRate(30);
+        parameters.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE);
+        //camera.setParameters(parameters);
+
+        try {
+            camera.setPreviewDisplay(mSurfaceHolder);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        camera.startPreview();
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+    }
+
+
+    //Getting permissions request Result
+    @Override
+    public void onRequestPermissionsResult (int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults){
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case CAMERA_REQUEST_CODE: {
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    mSurfaceHolder.addCallback(this);
+                    mSurfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+                }else{
+                    Toast.makeText(getContext(), "Please provide the permission", Toast.LENGTH_LONG).show();
+                }
+                break;
+            }
+        }
+    }
+
+    public void LogOut(){
+        FirebaseAuth.getInstance().signOut();
+        Intent intent  = new Intent(getContext(), SplashScreenActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+        return;
+    }
+
 }
