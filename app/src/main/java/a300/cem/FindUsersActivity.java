a2d4701e65f24e04840bb7dfe7154e59164e58c0
Bogdan A. Stanciu <stanciub@uni.coventry.ac.uk>
2018-12-04 14:36:38 +0000

    Search for users - in Firebase Database

--- app/src/main/java/a300/cem/FindUsersActivity.java
+++ app/src/main/java/a300/cem/FindUsersActivity.java
@@ -8,6 +8,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.storage.FirebaseStorage;
+
 import java.util.ArrayList;
 
 import a300.cem.RecyclerViewFollow.RcAdapter;
@@ -29,18 +38,86 @@ public class FindUsersActivity extends AppCompatActivity {
         mInput = findViewById(R.id.input);
         Button mSearch = findViewById(R.id.search);
 
+
+
         mRecyclerView = findViewById(R.id.recyclerView);
         mRecyclerView.setNestedScrollingEnabled(false);
+
+        //The recyclerView should not have a fixed size since the list can go up. A user can follow multiple 'friends'
         mRecyclerView.setHasFixedSize(false);
 
+        // use a linear layout manager
         mLayoutManager = new LinearLayoutManager(getApplication());
         mRecyclerView.setLayoutManager(mLayoutManager);
 
+        // specify an adapter (see also next example)
         mAdapter = new RcAdapter(getDataSet(), getApplication());
         mRecyclerView.setAdapter(mAdapter);
 
+        mSearch.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+
+                //Before searching in database, it requires to clear the data.
+                clear();
+                listenForData();
+            }
+        });
+
     }
 
+    private void listenForData(){
+        DatabaseReference userDb = FirebaseDatabase.getInstance().getReference().child("users");
+        //Query the database -> for the input text
+        Query query = userDb.orderByChild("email").startAt(mInput.getText().toString()).endAt(mInput.getText().toString() + "\uf8ff");
+        query.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                String email = "";
+                String uid = dataSnapshot.getRef().getKey();
+                if(dataSnapshot.child("email") != null){
+                    email = dataSnapshot.child("email").getValue().toString();
+
+                }
+                if(!email.equals(FirebaseAuth.getInstance().getCurrentUser().getUid())){
+                    UsersObject obj = new UsersObject(email,uid);
+                    results.add(obj);
+                    //After the data has changed we need to notify the adapter.
+                    mAdapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+
+    //Clear
+    public void clear(){
+        int size = this.results.size();
+        this.results.clear();
+        mAdapter.notifyItemRangeChanged(0,size);
+    }
+
+    //Get the results
     private ArrayList<UsersObject> results = new ArrayList<>();
     public ArrayList<UsersObject> getDataSet(){ return results;}
 
